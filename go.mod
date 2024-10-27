84989ba3104a885d0480f1a86cf863a2371a5592
David Dolgov <d.dolgov@jusan.kz>
2024-10-27 14:38:43 +0500

    added email and password hashing

--- go.mod
+++ go.mod
@@ -5,12 +5,13 @@ go 1.21.3
 require (
 	google.golang.org/grpc v1.67.1
 	google.golang.org/protobuf v1.35.1
+	github.com/lib/pq v1.10.9
+	golang.org/x/crypto v0.28.0
 )
 
 require (
-	github.com/lib/pq v1.10.9
 	golang.org/x/net v0.28.0 // indirect
-	golang.org/x/sys v0.24.0 // indirect
-	golang.org/x/text v0.17.0 // indirect
+	golang.org/x/sys v0.26.0 // indirect
+	golang.org/x/text v0.19.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240814211410-ddb44dafa142 // indirect
 )
--- services/user.go
+++ services/user.go
@@ -5,14 +5,20 @@ import (
 
 	db "github.com/dato7898/grpc-tube/db/sqlc"
 	"github.com/dato7898/grpc-tube/pb"
+	"github.com/dato7898/grpc-tube/util"
 )
 
 func (s *Server) Register(ctx context.Context, req *pb.RegisterRequest) (*pb.RegisterResponse, error) {
 
+	hashedPassword, err := util.HashPassword(req.Password)
+	if err != nil {
+		return nil, err
+	}
+
 	arg := db.CreateUserParams{
 		Username:       req.Username,
-		HashedPassword: req.Password,
-		Email:          "email",
+		HashedPassword: hashedPassword,
+		Email:          req.Email,
 	}
 
 	user, err := s.store.CreateUser(ctx, arg)
@@ -23,5 +29,6 @@ func (s *Server) Register(ctx context.Context, req *pb.RegisterRequest) (*pb.Reg
 	return &pb.RegisterResponse{
 		Id:       user.ID,
 		Username: user.Username,
+		Email:    user.Email,
 	}, nil
 }
